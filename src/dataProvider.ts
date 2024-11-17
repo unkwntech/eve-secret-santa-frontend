1b7caa45314f5501eac7b37aa2d0b5afecb830a8
Arron Chapman <unkwntech@gmail.com>
2024-11-17 16:06:37 -0600

    Added tokens to all reqests

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,5 +1,16 @@
 import simpleRestProvider from "ra-data-simple-rest";
+import { fetchUtils } from "react-admin";
 
 export const dataProvider = simpleRestProvider(
-  import.meta.env.VITE_SIMPLE_REST_URL,
+    import.meta.env.VITE_SIMPLE_REST_URL,
+    (url: string, options: any) => {
+        if (!options.headers) {
+            options.headers = new Headers({ Accet: "application/json" });
+        }
+        const token = localStorage.getItem("jwt");
+        options.headers.set("Authorization", `Bearer ${token}`);
+
+        return fetchUtils.fetchJson(url, options);
+        //https://marmelab.com/react-admin/doc/2.9/Authentication.html#sending-credentials-to-the-api
+    }
 );
