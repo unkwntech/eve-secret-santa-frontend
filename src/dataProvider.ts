53d23e84e2e1ef224e2bf72a5f079169adb3e566
Arron Chapman <unkwntech@gmail.com>
2024-11-17 20:04:09 -0600

    Started custom dataprovider

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,16 +1,156 @@
-import simpleRestProvider from "ra-data-simple-rest";
-import { fetchUtils } from "react-admin";
+import {
+    CreateParams,
+    CreateResult,
+    DeleteManyParams,
+    DeleteParams,
+    GetListParams,
+    GetListResult,
+    GetManyParams,
+    GetManyReferenceParams,
+    GetManyReferenceResult,
+    GetManyResult,
+    GetOneParams,
+    GetOneResult,
+    UpdateManyParams,
+    UpdateParams,
+} from "react-admin";
 
-export const dataProvider = simpleRestProvider(
-    import.meta.env.VITE_SIMPLE_REST_URL,
-    (url: string, options: any) => {
-        if (!options.headers) {
-            options.headers = new Headers({ Accet: "application/json" });
-        }
+// export const dataProvider = simpleRestProvider(
+//     import.meta.env.VITE_SIMPLE_REST_URL,
+//     (url: string, options: any) => {
+//         if (!options.headers) {
+//             options.headers = new Headers({ Accet: "application/json" });
+//         }
+//         const token = localStorage.getItem("jwt");
+//         options.headers.set("Authorization", `Bearer ${token}`);
+//         return fetchUtils.fetchJson(url, options);
+//         //https://marmelab.com/react-admin/doc/2.9/Authentication.html#sending-credentials-to-the-api
+//     }
+// );
+
+const secretSantaDataProvider = {
+    getList: async (
+        resource: string,
+        params: GetListParams
+    ): Promise<GetListResult> => {
         const token = localStorage.getItem("jwt");
-        options.headers.set("Authorization", `Bearer ${token}`);
+        if (!token) return Promise.reject();
 
-        return fetchUtils.fetchJson(url, options);
-        //https://marmelab.com/react-admin/doc/2.9/Authentication.html#sending-credentials-to-the-api
-    }
-);
+        return fetch(`${import.meta.env.VITE_SIMPLE_REST_URL}/${resource}/`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((res: any[]) => {
+                return {
+                    data: res,
+                    total: res.length,
+                    meta: {},
+                };
+            });
+    },
+    getOne: async (
+        resource: string,
+        params: GetOneParams
+    ): Promise<GetOneResult> => {
+        const token = localStorage.getItem("jwt");
+        if (!token) return Promise.reject();
+
+        return fetch(
+            `${import.meta.env.VITE_SIMPLE_REST_URL}/${resource}/${params.id}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        )
+            .then((res) => res.json())
+            .then((res: any) => {
+                return {
+                    data: res,
+                    total: res.length,
+                    meta: {},
+                };
+            });
+    },
+    getMany: async (
+        resource: string,
+        params: GetManyParams
+    ): Promise<GetManyResult> => {
+        const token = localStorage.getItem("jwt");
+        if (!token) return Promise.reject();
+
+        return fetch(
+            `${import.meta.env.VITE_SIMPLE_REST_URL}/${resource}/${params.ids}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        )
+            .then((res) => res.json())
+            .then((res: any[]) => {
+                console.log({
+                    data: res,
+                    total: res.length,
+                    meta: {},
+                });
+                return {
+                    data: [res],
+                    total: res.length,
+                    meta: {},
+                };
+            });
+    },
+    getManyReference: async (
+        resource: string,
+        params: GetManyReferenceParams
+    ): Promise<GetManyReferenceResult> => {
+        const token = localStorage.getItem("jwt");
+        if (!token) return Promise.reject();
+
+        return fetch(
+            `${import.meta.env.VITE_SIMPLE_REST_URL}/${resource}/${params.id}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        )
+            .then((res) => res.json())
+            .then((res: any) => {
+                return {
+                    data: res,
+                    total: res.length,
+                    meta: {},
+                };
+            });
+    },
+    create: (resource: string, params: CreateParams): Promise<CreateResult> => {
+        console.error("create not yet implimented");
+        return Promise.reject();
+    },
+
+    update: (resource: string, params: UpdateParams): Promise<any> => {
+        console.error("update not yet implimented");
+        return Promise.reject();
+    },
+
+    updateMany: (resource: string, params: UpdateManyParams): Promise<any> => {
+        console.error("udpate many not yet implimented");
+        return Promise.reject();
+    },
+
+    delete: (resource: string, params: DeleteParams): Promise<any> => {
+        console.error("delete not yet implimented");
+        return Promise.reject();
+    },
+
+    deleteMany: (resource: string, params: DeleteManyParams): Promise<any> => {
+        console.error("delete not yet implimented");
+        return Promise.reject();
+    },
+};
+
+export const dataProvider = secretSantaDataProvider;
